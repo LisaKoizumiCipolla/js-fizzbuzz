d4183d14d415db485fcf550e5c5081e066436439
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-05-16 16:09:55 +0200

    Add message

--- JS/script.js
+++ JS/script.js
@@ -3,27 +3,33 @@ const ulElement = document.querySelector("ul");
 
 let result;
 
+let message;
+
+let colour;
+
 for ( let index = 0 ; index < 31 ; index = index + 1){
     
     let li = document.createElement("li");
 
     if  (index % 3 === 0 && index % 5 === 0) {
-        console.log("fizzbuzz");
+        message = ("fizzbuzz");
         result = ("fizzbuzz");
         colour = "green";
     } else if (index % 5 === 0){
-        console.log("buzz");
+        message = ("buzz");
         result = ("buzz");
         colour = "blue";
     } else if (index % 3 === 0){
-        console.log("fizz");
+        message = ("fizz");
         result = ("fizz");
         colour = "yellow";
     } else {
-        console.log(index);
+        message = (index);
         result = index;
         colour = "black"
     }
+
+    console.log(message);
 
     li.classList.add(colour)
 
