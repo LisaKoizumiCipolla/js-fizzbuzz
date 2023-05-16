ec64ea3079ebc8f9beb5ecdfabaab006ba073df8
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-05-16 14:36:31 +0200

    Bonus 1

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,26 @@
 
+const ulElement = document.querySelector("ul");
+
+let result;
+
 for ( let index = 0 ; index < 31 ; index = index + 1){
 
     if  (index % 3 === 0 && index % 5 === 0) {
-        console.log("fizzbuzz")
+        console.log("fizzbuzz");
+        result = ("fizzbuzz");
     } else if (index % 5 === 0){
-        console.log("buzz")
+        console.log("buzz");
+        result = ("buzz");
     } else if (index % 3 === 0){
-        console.log("fizz")
+        console.log("fizz");
+        result = ("fizz");
     } else {
-        console.log(index)
+        console.log(index);
+        result = index;
     }
+
+    const li = document.createElement("li");
+    li.append("You got a " + result);
+
+    ulElement.appendChild(li);
 }
