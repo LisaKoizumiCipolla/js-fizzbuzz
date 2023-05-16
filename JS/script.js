a6b2c59c67fc865219463fffb1f93fcad0d5848a
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-05-16 13:44:53 +0200

    fix &&

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,12 @@
 
 for ( let index = 0 ; index < 31 ; index = index + 1){
 
-    if  (index % 3 === 0) {
-        console.log("fizz")
+    if  (index % 3 === 0 && index % 5 === 0) {
+        console.log("fizzbuzz")
     } else if (index % 5 === 0){
         console.log("buzz")
-    } else if (index % 3 === 0 && index % 5 === 0){
-        console.log("fizzbuzz")
+    } else if (index % 3 === 0){
+        console.log("fizz")
     } else {
         console.log(index)
     }
